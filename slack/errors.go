fa2c625373e3c6c4eb72764f68a54b1d9268cf72
Katharine Berry <ktbry@google.com>
2019-04-02 11:50:06 -0700

    fix problems again

--- slack/errors.go
+++ slack/errors.go
@@ -28,9 +28,9 @@ type ErrRateLimit struct {
 func (e ErrRateLimit) Error() string {
 	return fmt.Sprintf("slack has rate limited us for the next %s", e.Wait)
 }
-d
+
 type ErrSlack struct {
-	Type    string
+	Type     string
 	Warnings []string
 }
 
